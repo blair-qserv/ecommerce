0745236b43231a44839a4baaa75d71c233192473
blair-qserv <blairjurgenchacon19@gmail.com>
2025-02-26 22:41:55 +0800

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [currenyState,setCurrentState] = useState('Sign up')
+
   return (
-    <div>Login</div>
+    <form className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
+      <div className='inline-flex items-center gap-2 mb-2 mt-10'>
+        <p className='prata-regular text-3xl'>{currenyState}</p>
+        <hr className='border-none h-[1.5px] w-8 bg-gray-800' />
+      </div>
+      {currenyState === 'Login' ? '' :
+      <input type="text" className='w-full px-3 py-2 border border-gray-800' placeholder='Name' required />  }
+      <input type="email" className='w-full px-3 py-2 border border-gray-800' placeholder='Email' required />
+      <input type="password" className='w-full px-3 py-2 border border-gray-800' placeholder='Password' required />
+
+        <div className='w-full flex justify-between text-sm mt-[-8px] '>
+          <p className='cursor-pointer'>Forgot your password?</p>
+          {
+            currenyState === 'Login' ? 
+            <p onClick={()=>setCurrentState('Sign Up')} className='cursor-pointer'>Create Account</p>
+            : <p onClick={()=>setCurrentState('Login')}  className='cursor-pointer'>Login Here</p>
+          }
+        </div>
+          
+          <button className='bg-black text-white font-light px-8 py-2 mt-4'>{currenyState=== ' Login' ? 'Sign in': 'Sign up'}</button>
+
+    </form>
   )
 }
 
